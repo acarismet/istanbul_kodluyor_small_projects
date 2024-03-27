b6b1a2b45d7b9dba45249c74033ac641b7b5d25b
Ismet Acar <ismetacar@Ismets-Air.home>
2024-03-27 15:52:10 +0300

    teachers module created

---
+++ march_27_2024/teachers.py
@@ -0,0 +1,17 @@
+class Teacher:
+    def __init__(self, id, first_name, last_name, field):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.field = field
+
+    def update(self, first_name=None, last_name=None, field=None):
+        if first_name:
+            self.first_name = first_name
+        if last_name:
+            self.last_name = last_name
+        if field:
+            self.field = field
+
+    def __str__(self):
+        return f"Teacher ID: {self.id}, Name: {self.first_name} {self.last_name}, Field: {self.field}"
